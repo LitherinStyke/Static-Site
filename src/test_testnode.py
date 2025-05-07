76acb7ecddf9f19a971a4d319015038254be1e4e
LitherinStyke <li_styke@outlook.com>
2025-05-06 19:14:12 -0700

    Added basic unit text

---
+++ src/test_testnode.py
@@ -0,0 +1,19 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        
+        node3 = TextNode("This is a text node", TextType.LINK, "https://www.boot.dev")
+
+        self.assertEqual(node, node2)
+        self.assertNotEqual(node, node3)
+        self.assertIsNotNone(node3.url)
+
+
+if __name__ == "__main__":
+    unittest.main()
